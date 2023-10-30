3a828a538716fecff8f2e5353d19d88ae778339e
George <gnjuki19@gmail.com>
2023-10-30 17:55:56 +0300

    Worked on the getByRole method

---
+++ src/components/application/Application.test.js
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import Application from "./Application";
+
+describe("Application", () => {
+  test("Renders correctly!", () => {
+    render(<Application />);
+
+    const pageHeading = screen.getByRole("heading", {
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole("heading", { level: 2 });
+    expect(sectionHeading).toBeInTheDocument();
+
+    const nameElement = screen.getByRole("textbox", { name: "Name" });
+    expect(nameElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", { name: "Bio" });
+    expect(bioElement).toBeInTheDocument();
+
+    const jobLocationElement = screen.getByRole("combobox");
+    expect(jobLocationElement).toBeInTheDocument();
+
+    const termsElement = screen.getByRole("checkbox");
+    expect(termsElement).toBeInTheDocument();
+
+    const buttonElement = screen.getByRole("button");
+    expect(buttonElement).toBeInTheDocument();
+  });
+});
