badc46bd1a0a9e4c62d730a755687335c64e15f2
George <gnjuki19@gmail.com>
2023-10-30 14:45:33 +0300

    Learned about the test and describe blocks

--- src/Greet/Greet.test.js
+++ src/Greet/Greet.test.js
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("Greet renders correctly!", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
+describe("Greetings", () => {
+  test("Renders correctly!", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 });
 
-test("Greet renders correctly with wwoaname prop!", () => {
-  render(<Greet name="George" />);
-  const textElement = screen.getByText(/Hello George/i);
-  expect(textElement).toBeInTheDocument();
+describe("Nested", () => {
+  test("Renders correctly with a name prop!", () => {
+    render(<Greet name="George" />);
+    const textElement = screen.getByText(/Hello George/i);
+    expect(textElement).toBeInTheDocument();
+  });
 });
