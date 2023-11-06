1abf1dabf93323e9571f9b8cc2c2ecc52faec64e
George <gnjuki19@gmail.com>
2023-11-06 12:13:02 +0300

    Building a small static app to test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import List from "./components/birthday-reminder/List";
 // import { CounterTwo } from "./components/counter-two/CounterTwo";
 // import Application from "./components/application/Application";
 // import Counter from "./components/counter/Counter";
@@ -11,16 +12,27 @@ function App() {
   // const skills = ["HTML", "CSS", "JavaScript", "React"];
 
   return (
-    <AppProviders>
-      <div className="App">
-        {/* <Counter /> */}
-        {/* <Application /> */}
-        {/* <Skills skills={skills} /> */}
-        {/* <MuiMode /> */}
-        {/* <CounterTwo count={1} /> */}
-        <Users />
-      </div>
-    </AppProviders>
+    <div className="App">
+      <main>
+        <section className="container">
+          <h3>0 brithdays today!</h3>
+          <List />
+          <button onClick={() => console.log("I was clicked!")}>
+            Clear All
+          </button>
+        </section>
+      </main>
+    </div>
+    // <AppProviders>
+    //   <div className="App">
+    //     {/* <Counter /> */}
+    //     {/* <Application /> */}
+    //     {/* <Skills skills={skills} /> */}
+    //     {/* <MuiMode /> */}
+    //     {/* <CounterTwo count={1} /> */}
+    //     <Users />
+    //   </div>
+    // </AppProviders>
   );
 }
 
